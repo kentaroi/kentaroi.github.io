4ae11173d53bbb1ec1f752a11d4eafcc7b70add6
Kentaro Imai <kentaroi@gmail.com>
2022-01-17 23:16:14 +0900

    Update .eleventy.js to support scss

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,24 @@
+const sass = require("sass");
+const path = require("node:path");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addTemplateFormats("scss");
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+    compile: async function(inputContent, inputPath) {
+      let parsed = path.parse(inputPath);
+      if (parsed.name.startsWith("_")) {
+        return
+      }
+      let result = sass.compileString(inputContent, {
+        loadPaths: [parsed.dir || ".", this.config.dir.includes]
+      });
+      return async (data) => {
+        return result.css;
+      };
+    }
+  });
+
   return {
     dir: {
       input: "src",
