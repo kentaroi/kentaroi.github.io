2b6e2c5a320c45c7100541bd6a78179e012c17eb
Kentaro Imai <kentaroi@gmail.com>
2022-02-01 16:49:07 +0900

    Refactor styles with BEM methodology

--- src/_11ty/shortcodes.js
+++ src/_11ty/shortcodes.js
@@ -1,34 +1,41 @@
 const filters = require("./filters");
 module.exports = {
-  timeTagForYear: function(year) {
-    let url = `/${ year }/`;
-    return `<time class="year" datetime="${ year }"><a href="${ url }">${ year }</a></time>`;
+  linkToPost: function(post, classes="main__link") { // supposed to be used in main column
+    return `<a class="${ classes }" href="${ post.url }">${ post.data.title }</a>`;
   },
-  timeTagForMonth: function(year, month, showYear = true) {
+  timeTagForYear: function(year, klass = null) {
+    let classes = (klass ? klass + " " : "") + "year";
+    let url = `/${ year }/`;
+    return `<time class="${ classes }" datetime="${ year }"><a class="year__link" href="${ url }">${ year }</a></time>`;
+  },
+  timeTagForMonth: function(year, month, showYear = true, klass = null) {
+    let classes = (klass ? klass + " " : "") + "month";
     let name = filters.monthIndexToName(month);
     let numericMonth = filters.monthIndexToNumeric(month);
     let datetime = `${ year }-${ numericMonth }`
     let url = `/${ year }/${ numericMonth }/`;
-    let text = showYear ? name + ' ' + year.toString() : name;
-    return `<time class="month" datetime="${ datetime }"><a href="${ url }">${ text }</a></time>`;
+    let text = showYear ? name + " " + year.toString() : name;
+    return `<time class="${ classes }" datetime="${ datetime }"><a class="month__link" href="${ url }">${ text }</a></time>`;
   },
-  timeTagForDate: function(date, longOrShort = 'long') {
+  timeTagForDate: function(date, longOrShort, klass = null) {
+    let classes = (klass ? klass + " " : "") + "date";
     let datetime = date.toISOString();
     let formattedDate = filters.formatDate(date, longOrShort);
-    return `<time datetime="${ datetime }">${ formattedDate }</time>`;
+    return `<time class="${ classes }" datetime="${ datetime }">${ formattedDate }</time>`;
   },
-  tagList: function(tags) {
+  tagList: function(tags, klass = null) {
+    let classes = (klass ? klass + " " : "") + "tag-list";
     let tagLinks = tags.map(tag => {
       let url = filters.tagNameToUrl(tag);
-      return `<li><a href="${ url }" class="tag">${ tag }</a></li>`;
+      return `<li class="tag-list__tag tag"><a href="${ url }" class="tag__link">${ tag }</a></li>`;
     }).join("\n");
-    return '<ul class="tag-list">' + tagLinks + '</ul>';
+    return `<ul class="${ classes }">${ tagLinks }</ul>`;
   },
   tagCloud: function(weightedTags) {
     let listItems = weightedTags.map(tag => {
       let url = encodeURI("/tags/" + tag.name + "/");
-      return `<li><a href="${ url }" class="tag" data-weight="${ tag.weight }">${ tag.name }</a></li>`;
+      return `<li class="tag-cloud__tag tag"><a href="${ url }" class="tag__link" data-weight="${ tag.weight }">${ tag.name }</a></li>`;
     }).join("\n");
     return '<ul class="tag-cloud">' + listItems + '</ul>';
   }
-}
+};
